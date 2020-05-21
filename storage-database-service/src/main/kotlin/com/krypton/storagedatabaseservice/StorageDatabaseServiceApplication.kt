a18673767cc4829a2392c272fbaa4f0341fc2702
Krypton17 <maxdodon25@gmail.com>
2020-05-21 19:21:02 +0300

    initial commit

---
+++ storage-database-service/src/main/kotlin/com/krypton/storagedatabaseservice/StorageDatabaseServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.krypton.storagedatabaseservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StorageDatabaseServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<StorageDatabaseServiceApplication>(*args)
+}
---
+++ storage-database-service/src/test/kotlin/com/krypton/storagedatabaseservice/StorageDatabaseServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.krypton.storagedatabaseservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StorageDatabaseServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
