12410209c5ca5fa65a76adb04c30a82c361568e3
Krypton17 <maxdodon25@gmail.com>
2020-05-29 15:08:30 +0300

    Added transaction manager bean

--- storage-database-service/src/main/kotlin/com/krypton/storagedatabaseservice/config/MongoConfig.kt
+++ storage-database-service/src/main/kotlin/com/krypton/storagedatabaseservice/config/MongoConfig.kt
@@ -1,12 +1,15 @@
 package com.krypton.storagedatabaseservice.config
 
 import com.krypton.storagedatabaseservice.repository.FileRepository
+import com.krypton.storagedatabaseservice.repository.FolderRepository
 import com.mongodb.reactivestreams.client.MongoClient
 import com.mongodb.reactivestreams.client.MongoClients
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.MongoDbFactory
+import org.springframework.data.mongodb.MongoTransactionManager
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
-import com.krypton.storagedatabaseservice.repository.FolderRepository
 
 @Configuration
 @EnableReactiveMongoRepositories(basePackageClasses = [FolderRepository::class, FileRepository::class])
@@ -17,4 +20,7 @@ class MongoConfig : AbstractReactiveMongoConfiguration()
 	override fun reactiveMongoClient() : MongoClient = MongoClients.create()
 
 	override fun getDatabaseName() : String = "cloud-storage"
+
+	@Bean
+	fun transactionManager(dbFactory: MongoDbFactory): MongoTransactionManager = MongoTransactionManager(dbFactory)
 }
