c1cb52de6f85b36fa942b19edc93e5f09ee13353
Krypton17 <maxdodon25@gmail.com>
2020-05-29 15:07:22 +0300

    Folder repository

--- storage-database-service/src/main/kotlin/com/krypton/storagedatabaseservice/repository/FolderRepository.kt
+++ storage-database-service/src/main/kotlin/com/krypton/storagedatabaseservice/repository/FolderRepository.kt
@@ -1,7 +1,13 @@
 package com.krypton.storagedatabaseservice.repository
 
 import common.models.Folder
+import org.springframework.data.mongodb.repository.Query
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
 
 @Repository
 interface FolderRepository : FileEntityRepository<Folder, String>
+{
+	@Query("{ 'root' : ?0 }")
+	fun getRootFolder(root: Boolean) : Mono<Folder>
+}
