59a18341404c4b3e8d33ff2401b315e409c2d6a7
Krypton17 <maxdodon25@gmail.com>
2020-06-03 13:33:32 +0300

    id is now mutable var

--- common-models/src/main/kotlin/common/models/Folder.kt
+++ common-models/src/main/kotlin/common/models/Folder.kt
@@ -3,7 +3,7 @@ package common.models
 import java.util.UUID
 
 data class Folder(
-  val id          : String = UUID.randomUUID().toString(),
+  var id          : String = UUID.randomUUID().toString(),
   var name        : String,
   var path        : String,
   var folder      : String,    // folder id
