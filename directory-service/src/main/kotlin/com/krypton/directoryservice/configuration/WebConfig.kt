350d516ff08dd0db2e9f7f83c6b22924869e777c
Krypton17 <maxdodon25@gmail.com>
2020-06-07 19:05:57 +0300

    folder router

--- directory-service/src/main/kotlin/com/krypton/directoryservice/configuration/WebConfig.kt
+++ directory-service/src/main/kotlin/com/krypton/directoryservice/configuration/WebConfig.kt
@@ -1,6 +1,7 @@
 package com.krypton.directoryservice.configuration
 
 import com.krypton.directoryservice.router.FileRouter
+import com.krypton.directoryservice.router.FolderRouter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -9,4 +10,7 @@ class WebConfig
 {
 	@Bean
 	fun fileRouting(fileRouter: FileRouter) = fileRouter.router
+
+	@Bean
+	fun folderRouting(folderRouter: FolderRouter) = folderRouter.router
 }
