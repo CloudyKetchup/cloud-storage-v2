c8a4b4a4e835c9468b7a90626b8de1bc8c1e7a68
Krypton17 <maxdodon25@gmail.com>
2020-06-08 20:53:00 +0300

    added move and copy methods

--- directory-service/src/main/kotlin/com/krypton/directoryservice/client/file/IFileStorageClient.kt
+++ directory-service/src/main/kotlin/com/krypton/directoryservice/client/file/IFileStorageClient.kt
@@ -2,6 +2,7 @@ package com.krypton.directoryservice.client.file
 
 import com.krypton.directoryservice.client.IStorageClient
 import com.krypton.directoryservice.client.WebClientBodyResponse
+import com.krypton.directoryservice.model.FileMoveData
 import com.krypton.directoryservice.model.FileUploadResponse
 import common.models.File
 import org.springframework.http.codec.multipart.FilePart
@@ -12,4 +13,8 @@ interface IFileStorageClient : IStorageClient<File>
 		file: FilePart,
 		path: String
 	): WebClientBodyResponse<FileUploadResponse>
+
+	suspend fun move(moveData: FileMoveData): WebClientBodyResponse<File>
+
+	suspend fun copy(moveData: FileMoveData): WebClientBodyResponse<File>
 }
