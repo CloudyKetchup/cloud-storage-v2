9dc6d320fb4898bfd50a187f2eedab54ecb70fe2
Krypton17 <maxdodon25@gmail.com>
2020-06-11 13:33:57 +0300

    Folder router

---
+++ directory-service/src/main/kotlin/com/krypton/directoryservice/router/FolderRouter.kt
@@ -0,0 +1,19 @@
+package com.krypton.directoryservice.router
+
+import com.krypton.directoryservice.router.handler.FolderHandler
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.coRouter
+
+@Component
+class FolderRouter @Autowired constructor(handler: FolderHandler)
+{
+	val router = coRouter {
+		"/folder".nest {
+			POST("/create", handler::create)
+			DELETE("/delete", handler::delete)
+			PUT("/move", handler::move)
+			PUT("/copy", handler::copy)
+		}
+	}
+}
