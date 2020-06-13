f25e6c2b7eb54d0ffda2efa3eeb7251e5d601aa1
Krypton17 <maxdodon25@gmail.com>
2020-06-13 17:56:34 +0300

    using app context

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import MainLeftPanel  from './components/MainLeftPanel/MainLeftPanel';
 import Main           from './components/Main/Main';
 
-import { ThemeProvider } from './context/ThemeContext';
+import { AppContext }       from './context/AppContext';
+import { DirectoryContext } from './context/DirectoryContext';
+import { ThemeProvider }    from './context/ThemeContext';
+
+import FolderClient from './api/FolderClient';
 
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <ThemeProvider>
-      <MainLeftPanel />
-      <Main/>
-    </ThemeProvider>
-  </div>
-);
+const App = () =>
+{
+  const { setLoading, setErrorLoadingApp } = useContext(AppContext);
+  const { setFolder } = useContext(DirectoryContext);
+  const folderClient  = FolderClient.instance();
+
+  const onAppError = () =>
+  {
+    setLoading(false);
+    setErrorLoadingApp(true);
+  };
+
+  useEffect(() =>
+  {
+    folderClient.getRoot(onAppError).then(root => root && setFolder(root));
+  }, []);
+
+  return (
+    <div className="App">
+      <ThemeProvider>
+        <MainLeftPanel />
+        <Main />
+      </ThemeProvider>
+    </div>
+  );
+};
 
 export default App;
