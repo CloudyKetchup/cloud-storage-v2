ba265bbfa885d4cc12bbc4d1b2f78fb112657156
Krypton17 <maxdodon25@gmail.com>
2020-06-16 23:53:30 +0300

    onClick now performs setFolder in DirectoryContext

--- web-app/src/components/NavBar/NavFolder.jsx
+++ web-app/src/components/NavBar/NavFolder.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { DirectoryContext } from "../../context/DirectoryContext";
 
 import { ReactComponent as Arrow } 		from "../../assets/icons/arrow-right.svg"
 import { ReactComponent as HomeSvg } 	from "../../assets/icons/home.svg";
@@ -7,12 +9,15 @@ import './nav-bar.css';
 
 const NavFolder = ({ folder, last }) =>
 {
+	const { setFolder } = useContext(DirectoryContext);
 	const { name, root } = folder;
 
 	return (
-		<div className='nav-folder' >
+		<div className='nav-folder' onClick={() => setFolder(folder)}>
 			<div>
-				<span>{ !root ? name : <HomeSvg style={{ marginTop : "5px" }}/> }</span>
+				<span style={{ cursor : "pointer" }}>
+					{ !root ? name : <HomeSvg style={{ marginTop : "5px" }}/> }
+				</span>
 			</div>
 			<div>
 				{ !last && <Arrow className='nav-folder-arrow' /> }
