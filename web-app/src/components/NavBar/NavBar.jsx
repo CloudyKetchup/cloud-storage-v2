aa2415dbc18b5f78a2c17768d04ab04729a67eb0
Krypton17 <maxdodon25@gmail.com>
2020-06-16 21:36:14 +0300

    fetching previous folders and rendering NavFolders

--- web-app/src/components/NavBar/NavBar.jsx
+++ web-app/src/components/NavBar/NavBar.jsx
@@ -1,51 +1,83 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
-import { ThemeContext, Theme } from "../../context/ThemeContext";
+import NavFolder from './NavFolder';
 
-import { ReactComponent as BurgerSvg }  from "../../assets/icons/three-bars-menu.svg";
-import { ReactComponent as MoonSvg }    from "../../assets/icons/moon.svg";
-import { ReactComponent as SunSvg }     from "../../assets/icons/sun.svg";
+import FolderClient 						from '../../api/FolderClient';
+import { ThemeContext, Theme } 	from "../../context/ThemeContext";
+import { DirectoryContext } 		from '../../context/DirectoryContext';
+
+import { ReactComponent as BurgerSvg } 	from "../../assets/icons/three-bars-menu.svg";
+import { ReactComponent as MoonSvg } 		from "../../assets/icons/moon.svg";
+import { ReactComponent as SunSvg } 		from "../../assets/icons/sun.svg";
+
+import { isLastById } from '../../utils/array.utils';
 
 import './nav-bar.css';
 
 const NavBar = () =>
 {
-    const { theme, setTheme } = useContext(ThemeContext);
+	const { folder } = useContext(DirectoryContext);
+	const { theme, setTheme } = useContext(ThemeContext);
+	const [folders, setFolders] = useState([]);
 
-    const darkStyle = {
-        background  : Theme.DARK,
-        color       : Theme.LIGHT,
-        fill        : Theme.LIGHT
-    };
-    const lightStyle = {
-        background  : Theme.LIGHT,
-        color       : Theme.DARK,
-        fill        : Theme.DARK
-    };
+	const folderClient = FolderClient.instance();
 
-    const style = theme === Theme.LIGHT ? lightStyle : darkStyle;
+	useEffect(() =>
+	{
+		const fetchFolders = async () =>
+		{
+			if (folder)
+			{
+				const { data } = await folderClient.getPreviousFolders(folder.id);
 
-    return (
-        <div className="nav-bar" style={style}>
-            <div>
-                {/* NavFolders here */}
-            </div>
-            <div className="nav-bar-control">
-                <div>
-                    {
-                        theme === Theme.LIGHT
-                        ?
-                        <MoonSvg onClick={() => setTheme(Theme.DARK)}/>
-                        :
-                        <SunSvg onClick={() => setTheme(Theme.LIGHT)}/>
-                    }
-                </div>
-                <div>
-                    <BurgerSvg />
-                </div>
-            </div>
-        </div>
-    );
+				data && setFolders(data);
+			}
+		};
+
+		fetchFolders();
+	}, [folder]);
+
+	const darkStyle = {
+		background: Theme.DARK,
+		color			: Theme.LIGHT,
+		fill			: Theme.LIGHT
+	};
+
+	const lightStyle = {
+		background: Theme.LIGHT,
+		color			: Theme.DARK,
+		fill			: Theme.DARK
+	};
+
+	const style = theme === Theme.LIGHT ? lightStyle : darkStyle;
+
+	return (
+		<div className="nav-bar" style={style}>
+			<div>
+				{folders.map(folder =>
+					<NavFolder
+						key={folder.id}
+						folder={folder}
+						last={isLastById(folders, folder)}
+					/>
+				)}
+			</div>
+			<div className="nav-bar-control">
+				<div>
+					{
+						theme === Theme.LIGHT
+							?
+							<MoonSvg onClick={() => setTheme(Theme.DARK)} />
+							:
+							<SunSvg onClick={() => setTheme(Theme.LIGHT)} />
+					}
+				</div>
+				<div>
+					<BurgerSvg />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default NavBar;
