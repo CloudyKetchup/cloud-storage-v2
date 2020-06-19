fdaca54af62693eb9e332ac786dd50b5e272df1a
CloudyKetchup <maxdodon25@gmail.com>
2020-06-19 14:06:53 +0300

    using new DirectoryItemContextMenu

--- web-app/src/components/FolderItem/FolderItem.tsx
+++ web-app/src/components/FolderItem/FolderItem.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useEffect, useState, useContext } from "react";
 
-import ContextMenu, { ContextMenuActions } from "./ContextMenu";
+import ContextMenu, { ContextMenuActions } from "../DirectoryItemContextMenu/ContextMenu";
 
 import { Folder } 							from "../../models/Directory";
 import { ThemeContext, Theme } 	from "../../context/ThemeContext";
@@ -12,73 +12,71 @@ import { ReactComponent as CloseSvg }		from "../../assets/icons/delete.svg";
 
 import "./folder-item.css";
 
-//TODO: directory items context for only one options menu per child
-
 type IProps = { data: Folder };
 
 const FolderItem: FC<IProps> = ({ data }) =>
 {
-	// const [menu, setMenu] = useState<boolean>(false);
 	const { menuId, setMenuId } = useContext(ContextMenuContext);
 	const { theme } 						= useContext(ThemeContext);
 	const { id, name } 					= data;
 
 	useEffect(() =>
 	{
-		const optionsHover = async () =>
+		const div = document.getElementById(`folder-item-${id}`);
+
+		if (div)
 		{
-			const div 		= document.getElementById(`folder-item-${id}`);
-			const options = document.getElementById(`folder-item-${id}-options`);
-
-			if (div && options)
+			const onContextMenu = (e: Event) =>
 			{
-				const onOptionsHover = async () =>
-				{
-					options.style.display = "unset";
-				};
+				e.preventDefault();
 
-				const outOptionsHover = async () =>
-				{
-					options.style.display = "none";
-				};
+				toggleMenu();
+			};
 
-				div.addEventListener("mouseover", onOptionsHover);
-				div.addEventListener("mouseout", outOptionsHover);
+			div.addEventListener("contextmenu", onContextMenu);
 
-				return () =>
-				{
-					div.removeEventListener("mouseover", onOptionsHover);
-					div.removeEventListener("mouseout", outOptionsHover);
-				};
-			}
+			return () =>
+			{
+				div.removeEventListener("contextmenu", onContextMenu);
+			};
 		}
-
-		optionsHover();
-	}, []);
+	});
 
 	const substringName = (name: string): string =>
 	{
 		return name.length > 13 ? `${name.substring(0, 12)}...` : name;
 	};
 
-	const onCopy = async (id: string) =>
+	const onDownload = () =>
 	{
 		// TODO: implement
 	};
 
-	const onCut = async (id: string) =>
+	const onCopy = () =>
 	{
 		// TODO: implement
 	};
 
-	const onDelete = async (id: string) =>
+	const onCut = () =>
+	{
+		// TODO: implement
+	};
+
+	const onTrash = () =>
+	{
+		// TODO: implement
+	};
+
+	const onDelete = () =>
 	{
 		// TODO: implement
 	};
 
 	const actions: ContextMenuActions = {
+		onDownload : onDownload,
 		onCut 	: onCut,
 		onCopy	: onCopy,
+		onTrash : onTrash,
 		onDelete: onDelete
 	};
 
@@ -98,32 +96,24 @@ const FolderItem: FC<IProps> = ({ data }) =>
 			id={`folder-item-${id}`}
 			className={`folder-item ${ theme === Theme.DARK && "folder-item-dark" }`}
 		>
-			<div className="folder-item-icon">
-				<div>
-					<FolderSvg />
-				</div>
-			</div>
-			<div className="folder-item-text">
-				<span>{substringName(name)}</span>
-			</div>
-			<div
-				id={`folder-item-${id}-options`}
-				className="folder-item-options"
-				style={{ display : "none" }}
-				onClick={toggleMenu}
-			>
-				{
-					menuId === id
-					?
-					<CloseSvg fill={ theme === Theme.LIGHT ? "#181818" : "white" }/>
-					:
-					<DotsSvg fill={ theme === Theme.LIGHT ? "#181818" : "white" }/>
-				}
-			</div>
+			{
+				menuId !== id
+				&&
+				<>
+					<div className="folder-item-icon">
+						<div>
+							<FolderSvg />
+						</div>
+					</div>
+					<div className="folder-item-text">
+						<span>{substringName(name)}</span>
+					</div>
+				</>
+			}
 			{
 				menuId === id
 				&&
-				<ContextMenu folderId={id} actions={actions}/>
+				<ContextMenu actions={actions} className="folder-item-contextmenu"/>
 			}
 		</div>
 	);
